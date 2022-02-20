5b329a95fe2b87cd3980968f72b117931bf057c8
Adriana Čejka <adriana.cejka@seznam.cz>
2022-02-20 16:44:34 +0100

    task No. 1 - bolder text

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-// tady je místo pro náš program
+// tady je místo pro náš program  
+
+
+let nadpis = document.querySelector("h1");
+
+
+
+function priNajeti() {
+    nadpis.classList.toggle("hover");
+    lev.classList.toggle("bold");
+}
+
+function poStisknuti(udalost) {
+    console.log(udalost.key);
+}
+
+function startAudio() {
+    let audioFile = document.querySelector("#zvukovaStopa");
+    console.log("Zpínám píseň...")
+    audioFile.play();
+}
+
+let lev = document.querySelector(".lev");
+
+
